f09ac045b6a6441b70c3b624eb33a8b49dc730d0
jyritasa <Jyri.tasala@gmail.com>
2023-06-12 11:27:13 +0300

    Added more documentation. Removed unnesecary variable Color _c

--- lib/components/settings_manger.dart
+++ lib/components/settings_manger.dart
@@ -24,18 +24,22 @@ class SettingsManager {
     return _settingsManager;
   }
 
-  static const Color _c = initialSeedColor;
   static const String _box = "settings";
   static const String _colorKey = "color";
   static const String _localeKey = "locale";
   static const String _showIntroKey = "showIntro";
 
+  //Hive deals with variables as dynamic. It can handle bools but [int]s and
+  //[String]s are typed as [dynamic]. This affects: [_color], [_locale],
+
   bool _setupDone = false;
+  //List<int>
   List<dynamic> _color = [
     initialSeedColor.red,
     initialSeedColor.green,
     initialSeedColor.blue
   ];
+  //String
   dynamic _locale = "en";
   bool _showIntro = true;
 
@@ -49,13 +53,20 @@ class SettingsManager {
   Future<void> init() async {
     await Hive.initFlutter();
     var settings = await Hive.openBox(_box);
-    _color = await settings
-        .get(_colorKey, defaultValue: [_c.red, _c.green, _c.blue]);
+    _color = await settings.get(
+      _colorKey,
+      defaultValue: [
+        initialSeedColor.red,
+        initialSeedColor.green,
+        initialSeedColor.blue,
+      ],
+    );
     _locale = await settings.get(_localeKey, defaultValue: "en");
     _showIntro = await settings.get(_showIntroKey, defaultValue: true);
     settings.close();
   }
 
+  ///Save RGB value for theme seed color and background gradient into Hive.
   Future<void> setColor(int r, int g, int b) async {
     _color = [r, g, b];
     var settings = await Hive.openBox(_box);
@@ -63,13 +74,15 @@ class SettingsManager {
     settings.close();
   }
 
-  Future<void> setLocale(locale) async {
+  ///Save app locale as String ("en", "fi", etc) into Hive.
+  Future<void> setLocale(String locale) async {
     _locale = locale;
     var settings = await Hive.openBox(_box);
     await settings.put(_localeKey, locale);
     settings.close();
   }
 
+  ///Marking Intro having been shown and wont be shown again at launch.
   Future<void> disableIntro() async {
     _showIntro = false;
     var settings = await Hive.openBox(_box);
@@ -80,8 +93,10 @@ class SettingsManager {
   ///Updates color theme to one from [Hive], runs [showIntroDialog] and marks
   ///[_setupDone].
   ///
-  ///This is called after [HomePage] is built.
+  ///This is called after [HomePage] is built inside the build() function.
   void completeSetUp(WidgetRef ref, context) {
+    //Complete setup rebuilds the UI, this is to prevent post-setup from being
+    //run more than once.
     if (!_setupDone) {
       updateColorTheme(color[0], color[1], color[2], ref);
       if (showIntro) showIntroDialog(context, ref);
